915ee98d6ff6c8109545835bf7775b8b179b8043
leokf-ntnu <100607912+leokf-ntnu@users.noreply.github.com>
2023-10-31 12:47:45 +0100

    Add files via upload

---
+++ hangman.js
@@ -0,0 +1,17 @@
+//ordet som skal gjettes
+hemmeligOrd = "kake";
+
+//liste med bokstaver som er gjettet riktig
+svar = ["_", "_", "_", "_"]
+
+//antall liv
+liv = 6;
+
+//viser svar listen, og antall liv i html dokumentet
+document.getElementById("svar").innerHTML = svar;   
+document.getElementById("antallLiv").innerHTML = liv;
+
+
+function sjekkBokstav() {
+    //Fyll denne funksjonen, slik at den sjekker om bokstaven som er gjettet er i ordet
+}
