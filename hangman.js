1a9e544671432ed7ede1b8bd08bbc4114c6fffa9
SyverBerge <143180649+SyverBerge@users.noreply.github.com>
2023-11-03 12:22:02 +0100

    q293u23

--- hangman.js
+++ hangman.js
@@ -1,17 +1,24 @@
 //ordet som skal gjettes
-hemmeligOrd = "kake";
+let hemmeligOrd = "kake";
 
 //liste med bokstaver som er gjettet riktig
-svar = ["_", "_", "_", "_"]
+let svar = ["_", "_", "_", "_"]
 
 //antall liv
-liv = 6;
+let liv = 6;
 
 //viser svar listen, og antall liv i html dokumentet
-document.getElementById("svar").innerHTML = svar;   
-document.getElementById("antallLiv").innerHTML = liv;
 
 
 function sjekkBokstav() {
-    //Fyll denne funksjonen, slik at den sjekker om bokstaven som er gjettet er i ordet
-}
+    let bokstav = document.getElementById("gjettInput").value
+    for (var i = 0; i < hemmeligOrd.length; i++){
+        if (bokstav == hemmeligOrd[i]){
+            svar[i] = bokstav;
+        }
+    
+    }
+    
+    document.getElementById("antallLiv").innerHTML = liv;
+    document.getElementById("svar").innerHTML = svar;   
+    }
