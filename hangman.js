5c9d565f3686a008e67fa6c8b333b73a26a062aa
SyverBerge <143180649+SyverBerge@users.noreply.github.com>
2023-11-09 08:47:02 +0100

    ferdig

--- hangman.js
+++ hangman.js
@@ -1,24 +1,29 @@
 //ordet som skal gjettes
-let hemmeligOrd = "kake";
+hemmeligOrd = "kake";
 
 //liste med bokstaver som er gjettet riktig
-let svar = ["_", "_", "_", "_"]
+svar = ["_", "_", "_", "_"]
 
 //antall liv
-let liv = 6;
+liv = 6;
 
 //viser svar listen, og antall liv i html dokumentet
+document.getElementById("antallLiv").innerHTML = liv;
+document.getElementById("svar").innerHTML = svar;   
 
 
 function sjekkBokstav() {
-    let bokstav = document.getElementById("gjettInput").value
-    for (var i = 0; i < hemmeligOrd.length; i++){
-        if (bokstav == hemmeligOrd[i]){
-            svar[i] = bokstav;
+    bokstav = document.getElementById("gjettInput").value
+    if (hemmeligOrd.includes(bokstav)){
+        for (let i = 0; i < hemmeligOrd.length; i++){
+            if (bokstav == hemmeligOrd[i]){
+                svar[i] = bokstav;
+                document.getElementById("svar").innerHTML = svar;
+            }
         }
-    
     }
-    
-    document.getElementById("antallLiv").innerHTML = liv;
-    document.getElementById("svar").innerHTML = svar;   
+    else{
+        liv--;
+        document.getElementById("antallLiv").innerHTML = liv;
     }
+}
